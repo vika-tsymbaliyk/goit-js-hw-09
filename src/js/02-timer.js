0cda261fd5ac5c9a5a0f5f599b4139f5a4015cd1
vika-tsymbaliyk <vika.bronytska@gmail.com>
2023-07-11 18:04:08 +0300

    second

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,13 +3,64 @@ import "flatpickr/dist/flatpickr.min.css";
 
 const datePicker = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('[data-start]');
+const daysValue = document.querySelector("span[data-days]");
+const hoursValue = document.querySelector("span[data-hours]");
+const minutesValue = document.querySelector("span[data-minutes]");
+const secondsValue = document.querySelector("span[data-seconds]");
+const currentDate = new Date();
+let selectedDate = new Date();
 
+btnStart.addEventListener("click", timeCount);
 
+btnStart.disabled = true;
 
+flatpickr(datePicker, {
+    enableTime: true,
+    // dateFormat: "Y-m-d H:i",
+    time_24hr: true,
+    defaultDate: currentDate,
+    minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < currentDate) {
+      window.alert("Please choose a date in the future");
+      
+    } else {
+      // console.log(selectedDates[0])
+      btnStart.disabled = false;
+      selectedDate = selectedDates[0]
+    }
+    },
+})
 
+function timeCount() {
+  btnStart.disabled = true;
+  // dateTimeInput.disabled = true;
+  getTime();
+}
 
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+}
 
+function getTime() {
+    timerId = setInterval(() => {
+        const delta = selectedDate - Date.now();
+        const dataSet = convertMs(delta);
+        
+        if (delta <= 0) {
+             clearInterval(timerId);
+        } else {
+            showTime(dataSet);
+        }
+        }, 1000);
+}
 
+function showTime(dataSet) {
+    daysValue.textContent = addLeadingZero(dataSet.days);
+    hoursValue.textContent = addLeadingZero(dataSet.hours);
+    minutesValue.textContent = addLeadingZero(dataSet.minutes);
+    secondsValue.textContent = addLeadingZero(dataSet.seconds);
+}
 
 
 function convertMs(ms) {
@@ -30,7 +81,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
